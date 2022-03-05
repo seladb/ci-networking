a43da812363de35e39852696552037971e429acb
seladb <pcapplusplus@gmail.com>
2022-03-05 01:37:01 -0800

    Run tcpreplay before tcpdum

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import subprocess
+import time
 
+tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "1.pcap"])
 tcpdump_proc = subprocess.Popen("tcpdump")
-subprocess.run(["tcpreplay", "-i", "eth0", "--mbps=10", "1.pcap"])
+
+time.sleep(10)
+
 tcpdump_proc.kill()
-outs, errs = tcpdump_proc.communicate()
-
-print("ERRORS:")
-print(errs)
-
-print("STDOUT:")
-print(outs)
+tcpreplay_proc.kill()
