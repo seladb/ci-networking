c94af88b2e240a301f65d4e1b221351e96534dd9
seladb <pcapplusplus@gmail.com>
2022-03-06 01:47:49 -0800

    Test CentOS

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ ip_address= socket.gethostbyname(socket.gethostname())
 try:
   tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "-l", "0", "1.pcap"])
 
-  completed_process = subprocess.run(["Bin/Packet++Test"], cwd="PcapPlusPlus/Tests/Packet++Test")
+  completed_process = subprocess.run(["Bin/Packet++Test", "-s"], cwd="PcapPlusPlus/Tests/Packet++Test")
   if completed_process.returncode != 0:
     exit(completed_process.returncode)
 
-  completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address], cwd="PcapPlusPlus/Tests/Pcap++Test")
+  completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address, "-s"], cwd="PcapPlusPlus/Tests/Pcap++Test")
   if completed_process.returncode != 0:
     exit(completed_process.returncode)
 
