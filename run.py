bf3e68df2e61fdfbfa9041dce35a838a3174fdfa
seladb <pcapplusplus@gmail.com>
2022-03-08 01:22:37 -0800

    use netifaces

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import sys
 import subprocess
 import socket
+import netifaces as ni
 
 def main():
-  ip_address= socket.gethostbyname(socket.gethostname())
-  print(ip_address)
+  
+  ni.ifaddresses("en0")
+  ip = ni.ifaddresses("en0")[ni.AF_INET][0]["addr"]
+  print("IP ADDRESS OF EN0:")
+  print(ip)  # should print "192.168.100.37"
+
+  # ip_address= socket.gethostbyname(socket.gethostname())
+  # print(ip_address)
   subprocess.run("ifconfig")
   return
 
+  ip_address= socket.gethostbyname(socket.gethostname())
   try:
     tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "-l", "0", "1.pcap"])
 
