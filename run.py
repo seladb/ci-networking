bb9d544dc1be03665afb93e9190e3ec2a012b815
seladb <pcapplusplus@gmail.com>
2022-03-08 00:39:59 -0800

    - Add MacOS 11 - Add main to run.py

--- run.py
+++ run.py
@@ -2,19 +2,22 @@ import sys
 import subprocess
 import socket
 
-ip_address= socket.gethostbyname(socket.gethostname())
+def main():
+  ip_address= socket.gethostbyname(socket.gethostname())
 
-try:
-  tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "-l", "0", "1.pcap"])
+  try:
+    tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "-l", "0", "1.pcap"])
 
-  completed_process = subprocess.run(["Bin/Packet++Test"] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Packet++Test")
-  if completed_process.returncode != 0:
-    exit(completed_process.returncode)
+    completed_process = subprocess.run(["Bin/Packet++Test"] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Packet++Test")
+    if completed_process.returncode != 0:
+      exit(completed_process.returncode)
 
-  completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Pcap++Test")
-  if completed_process.returncode != 0:
-    exit(completed_process.returncode)
+    completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Pcap++Test")
+    if completed_process.returncode != 0:
+      exit(completed_process.returncode)
 
-finally:
-  tcpreplay_proc.kill()
+  finally:
+    tcpreplay_proc.kill()
 
+if __name__ == "__main__":
+    main()
