67c4b9d7df5ebb709809100c4b7f6c7d6f6deae3
seladb <pcapplusplus@gmail.com>
2022-03-08 01:49:23 -0800

    Try with IP address

--- run.py
+++ run.py
@@ -1,29 +1,23 @@
+from ast import arg
 import sys
 import subprocess
 import socket
 import netifaces as ni
 
 def main():
-  
-  ni.ifaddresses("en0")
-  ip = ni.ifaddresses("en0")[ni.AF_INET][0]["addr"]
-  print("IP ADDRESS OF EN0:")
-  print(ip)  # should print "192.168.100.37"
+  iface = sys.argv[1]
+  ip_address = ni.ifaddresses(iface)[ni.AF_INET][0]["addr"]
+  print("IP address is: %s" % ip_address)
 
   # ip_address= socket.gethostbyname(socket.gethostname())
-  # print(ip_address)
-  subprocess.run("ifconfig")
-  return
-
-  ip_address= socket.gethostbyname(socket.gethostname())
   try:
-    tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", "eth0", "--mbps=10", "-l", "0", "1.pcap"])
+    tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", iface, "--mbps=10", "-l", "0", "1.pcap"])
 
     completed_process = subprocess.run(["Bin/Packet++Test"] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Packet++Test")
     if completed_process.returncode != 0:
       exit(completed_process.returncode)
 
-    completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address] + sys.argv[1:], cwd="PcapPlusPlus/Tests/Pcap++Test")
+    completed_process = subprocess.run(["Bin/Pcap++Test", "-i", ip_address] + sys.argv[2:], cwd="PcapPlusPlus/Tests/Pcap++Test")
     if completed_process.returncode != 0:
       exit(completed_process.returncode)
 
