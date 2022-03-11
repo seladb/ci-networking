62a74c4d6c2ed612348d6fe80a126723932d64f6
seladb <pcapplusplus@gmail.com>
2022-03-11 01:12:39 -0800

    Test PcapPlusPlus

--- run_windows.py
+++ run_windows.py
@@ -7,27 +7,24 @@ def main():
   tcpreplay_interface = raw_nics_output.split("\n")[2].split("\t")[1]
   print("Interface is: %s" % tcpreplay_interface)
 
-  tcpreplay_cmd = f"tcpreplay.exe -i \"{tcpreplay_interface}\" --mbps=10 ..\\1.pcap"
-  subprocess.run(tcpreplay_cmd, shell=True, cwd="tcpreplay-win")
-
   ni_interface = tcpreplay_interface.lstrip("\\Device\\NPF_")
   ip_address = ni.ifaddresses(ni_interface)[ni.AF_INET][0]["addr"]
   print("IP address is: %s" % ip_address)
 
-  # try:
-  #   tcpreplay_proc = subprocess.Popen(["tcpreplay", "-i", args.interface, "--mbps=10", "-l", "0", "1.pcap"], cwd=args.tcpreplay_dir)
+  try:
+    tcpreplay_cmd = f"tcpreplay.exe -i \"{tcpreplay_interface}\" --mbps=10 -l 0 ..\\1.pcap"
+    tcpreplay_proc = subprocess.Popen(tcpreplay_cmd, shell=True, cwd="tcpreplay-win")
 
-  #   use_sudo = ["sudo"] if args.use_sudo else []
-  #   completed_process = subprocess.run(use_sudo + ["Bin/Packet++Test"] + args.test_args.split(), cwd="PcapPlusPlus/Tests/Packet++Test")
-  #   if completed_process.returncode != 0:
-  #     exit(completed_process.returncode)
+    completed_process = subprocess.run("Bin\\Packet++Test", cwd="PcapPlusPlus\\Tests\\Packet++Test")
+    if completed_process.returncode != 0:
+      exit(completed_process.returncode)
 
-  #   completed_process = subprocess.run(use_sudo + ["Bin/Pcap++Test", "-i", ip_address] + args.test_args.split(), cwd="PcapPlusPlus/Tests/Pcap++Test")
-  #   if completed_process.returncode != 0:
-  #     exit(completed_process.returncode)
+    completed_process = subprocess.run(["Bin\\Pcap++Test", "-i", ip_address], cwd="PcapPlusPlus\\Tests\\Pcap++Test")
+    if completed_process.returncode != 0:
+      exit(completed_process.returncode)
 
-  # finally:
-  #   tcpreplay_proc.kill()
+  finally:
+    tcpreplay_proc.kill()
 
 if __name__ == "__main__":
   main()
