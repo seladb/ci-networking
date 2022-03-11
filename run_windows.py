3659e5afcd044df41c4b18fc931d06aa80d24e61
seladb <pcapplusplus@gmail.com>
2022-03-11 02:00:33 -0800

    Run rpcapd

--- run_windows.py
+++ run_windows.py
@@ -12,28 +12,34 @@ def main():
   ip_address = ni.ifaddresses(ni_interface)[ni.AF_INET][0]["addr"]
   print("IP address is: %s" % ip_address)
 
-  try:
-    tcpreplay_cmd = f"tcpreplay.exe -i \"{tcpreplay_interface}\" --mbps=10 -l 0 ..\\1.pcap"
-    tcpreplay_proc = subprocess.Popen(tcpreplay_cmd, shell=True, cwd="tcpreplay-win")
+  completed_process = subprocess.run(
+    os.path.join(["rpcapd", "-b", ip_address, "-p", "12321"]),
+    cwd=os.path.join("PcapPlusPlus", "Tests", "Pcap++Test", "rpcapd"),
+    shell=True,
+  )
 
-    completed_process = subprocess.run(
-      os.path.join("Bin", "Packet++Test"),
-      cwd=os.path.join("PcapPlusPlus", "Tests", "Packet++Test"),
-      shell=True,
-    )
-    if completed_process.returncode != 0:
-      exit(completed_process.returncode)
+  # try:
+  #   tcpreplay_cmd = f"tcpreplay.exe -i \"{tcpreplay_interface}\" --mbps=10 -l 0 ..\\1.pcap"
+  #   tcpreplay_proc = subprocess.Popen(tcpreplay_cmd, shell=True, cwd="tcpreplay-win")
 
-    completed_process = subprocess.run(
-      [os.path.join("Bin", "Pcap++Test"), "-i", ip_address],
-      cwd=os.path.join("PcapPlusPlus", "Tests", "Pcap++Test"),
-      shell=True,
-    )
-    if completed_process.returncode != 0:
-      exit(completed_process.returncode)
+  #   completed_process = subprocess.run(
+  #     os.path.join("Bin", "Packet++Test"),
+  #     cwd=os.path.join("PcapPlusPlus", "Tests", "Packet++Test"),
+  #     shell=True,
+  #   )
+  #   if completed_process.returncode != 0:
+  #     exit(completed_process.returncode)
 
-  finally:
-    tcpreplay_proc.kill()
+  #   completed_process = subprocess.run(
+  #     [os.path.join("Bin", "Pcap++Test"), "-i", ip_address],
+  #     cwd=os.path.join("PcapPlusPlus", "Tests", "Pcap++Test"),
+  #     shell=True,
+  #   )
+  #   if completed_process.returncode != 0:
+  #     exit(completed_process.returncode)
+
+  # finally:
+  #   tcpreplay_proc.kill()
 
 if __name__ == "__main__":
   main()
